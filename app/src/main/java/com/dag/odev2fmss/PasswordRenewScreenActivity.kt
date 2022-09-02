dcde6fd7fad2ed2c3d483ce81d4b06e4df84cb3a
berkealemdaroglu <ersinberkealemdaroglu@gmail.com>
2022-09-02 19:26:08 +0300

    Necessary Activities Created. | Get Started Screen, Login Screen, Signup Screen, Password Renew Screen...

---
+++ app/src/main/java/com/dag/odev2fmss/PasswordRenewScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.dag.odev2fmss
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PasswordRenewScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_renew_screen)
+    }
+}
