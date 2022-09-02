5194a28062f4dc0982ea77403b2e8c9d3426b8e7
berkealemdaroglu <ersinberkealemdaroglu@gmail.com>
2022-09-03 02:14:49 +0300

    Login Screen & Create Account Screen Activities Design is Completed. | #Get Started Screen Activity Bug Fix.

--- app/src/main/java/com/dag/odev2fmss/GetStartedScreenActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/GetStartedScreenActivity.kt
@@ -1,11 +1,21 @@
 package com.dag.odev2fmss
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dag.odev2fmss.databinding.ActivityGetStartedScreenBinding
 
 class GetStartedScreenActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityGetStartedScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_started_screen)
+        binding = ActivityGetStartedScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.joinNowButton.setOnClickListener {
+            val intent = Intent(this, LoginScreenActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/dag/odev2fmss/LoginScreenActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/LoginScreenActivity.kt
@@ -1,11 +1,22 @@
 package com.dag.odev2fmss
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dag.odev2fmss.databinding.ActivityGetStartedScreenBinding
+import com.dag.odev2fmss.databinding.ActivityLoginScreenBinding
 
 class LoginScreenActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_screen)
+        binding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.createNewAccountButton.setOnClickListener {
+            val intent = Intent(this, CreateAccountScreenActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
